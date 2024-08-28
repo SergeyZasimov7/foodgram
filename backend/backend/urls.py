579239922eed0f07932cb37f3ee6316cd5356954
Сергей Засимов <zasimovsergey@yandex.ru>
2024-08-28 09:18:34 +0300

    6

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
+from api.views import ShortLinkViewSet
 from django.contrib import admin
 from django.urls import include, path
-
-from api.views import ShortLinkViewSet
 
 urlpatterns = [
     path(
